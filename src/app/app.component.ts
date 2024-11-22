79c82560833df7cd02d6f3bf7fc35f39316e024a
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2024-11-22 06:46:46 +0100

    chore(wepapp): remove firebase test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +8,4 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  readonly isStaging = !environment.production;
-}
+export class AppComponent {}

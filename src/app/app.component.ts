96e4154e9b690d7c27a24f5426f400ec3b466436
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2024-12-09 11:19:37 +0100

    Live coding 9 : walking skeleton & Firebase store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly #authenticationService = inject(AuthenticationService);
+
+  onLogin() {
+    this.#authenticationService
+      .login('john.doe@gmail.com', 'qwertz')
+      .pipe(
+        switchMap((response) => {
+          const { email, localId, idToken } = response;
+          return this.#authenticationService.save(email, localId, idToken);
+        }),
+      )
+      .subscribe((response) => {});
+  }
+}

101286f391bbb95a9d0bd20c413a0d3dc3af5d57
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-01-25 13:04:43 +0100

    core(config): set playwright test for production env

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:development',
+    command: 'npm run start:production',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env['CI'],
   },

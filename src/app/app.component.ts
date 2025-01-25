fbb0da8fad5495a7365472edd6b4c44e01f800d5
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-01-25 10:50:10 +0100

    Feature(HomePage): implement homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { switchMap } from 'rxjs';
-import { AuthenticationService } from '@core/services/authentication.service';
+import { HeaderComponent } from '@shared/components/header/header.smart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  readonly #authenticationService = inject(AuthenticationService);
-
-  onLogin() {
-    this.#authenticationService
-      .login('john.doe@gmail.com', 'qwertz')
-      .pipe(
-        switchMap((response) => {
-          const { email, localId, idToken } = response;
-          return this.#authenticationService.save(email, localId, idToken);
-        }),
-      )
-      .subscribe();
-  }
-}
+export class AppComponent {}

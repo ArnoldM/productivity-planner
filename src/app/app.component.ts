f7873b6598f9430951593496398c6c1b99c374cd
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-01-31 11:02:23 +0100

    refacto(header): move header component to core folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '@shared/components/header/header.smart.component';
+import { HeaderComponent } from '@core/components/header/header.smart.component';
 
 @Component({
   selector: 'app-root',

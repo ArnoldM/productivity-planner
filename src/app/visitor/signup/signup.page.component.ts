6c252297843be2159c0e0ca47454a6c452524d68
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-02-02 23:57:31 +0100

    feat(signup): implement Form

--- src/app/visitor/signup/signup.page.component.ts
+++ src/app/visitor/signup/signup.page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './signup.page.component.html',
   styleUrl: './signup.page.component.scss',
 })
-export default class SignupPageComponent {}
+export default class SignupPageComponent {
+  readonly name = signal<string>('');
+  readonly email = signal<string>('');
+  readonly password = signal<string>('');
+  readonly confirmPassword = signal<string>('');
+
+  readonly isPasswordMatchValid = computed(
+    () => this.password() === this.confirmPassword(),
+  );
+
+  onSubmit() {
+    console.log('form submitted');
+  }
+}

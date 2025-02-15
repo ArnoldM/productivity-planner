eed8760237f21e0f8e4d5fa282b581ce209e5e38
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-02-15 10:30:04 +0100

    chore(deps): Fix Lint action

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = tseslint.config(
   {
@@ -11,7 +12,6 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
-      'plugin:prettier/recommended',
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -38,4 +38,5 @@ module.exports = tseslint.config(
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
   },
+  eslintPluginPrettierRecommended,
 );

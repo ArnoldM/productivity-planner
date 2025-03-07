c3200b33bb43683c64d898f5af2adabafeea53a4
Arnold Messifet <arnold.messifet@nomadstudio.ch>
2025-03-07 08:41:15 +0100

    fix(register): fix RegisterUserUseCaseService TU

--- src/app/core/use-case/register-user.use-case.service.spec.ts
+++ src/app/core/use-case/register-user.use-case.service.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RegisterUserUseCaseService } from './register-user.use-case.service';
+import { AuthenticationService } from '@core/services/authentication.service';
+import { UserService } from '@core/repositories/user.service';
 
 describe('RegisterUserUseCaseService', () => {
   let service: RegisterUserUseCaseService;
+  let mockAuthService: jest.Mocked<AuthenticationService>;
+  let mockUserService: jest.Mocked<UserService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockAuthService = {
+      register: jest.fn(),
+    } as unknown as jest.Mocked<AuthenticationService>;
+
+    mockUserService = {
+      create: jest.fn(),
+    } as unknown as jest.Mocked<UserService>;
+
+    TestBed.configureTestingModule({
+      providers: [
+        RegisterUserUseCaseService,
+        { provide: AuthenticationService, useValue: mockAuthService },
+        { provide: UserService, useValue: mockUserService },
+      ],
+    });
     service = TestBed.inject(RegisterUserUseCaseService);
   });
 

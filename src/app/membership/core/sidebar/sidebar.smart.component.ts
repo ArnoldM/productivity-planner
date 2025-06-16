11e09ddfa5f29a95970142987a06d4961181a14e
Arnold Messifet <arnold.messifet@gmail.com>
2025-06-16 19:53:24 +0200

    feat(sidebar): add visual nav item on active route

--- src/app/membership/core/sidebar/sidebar.smart.component.ts
+++ src/app/membership/core/sidebar/sidebar.smart.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { APP_ROUTES } from '@core/models/enums/routes.enum';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.smart.component.html',
   styleUrl: './sidebar.smart.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

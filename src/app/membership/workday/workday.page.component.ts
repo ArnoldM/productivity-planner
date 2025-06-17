4ed4243d2581bed18c520cdeaf5b252c4f6fcd44
Arnold Messifet <arnold.messifet@gmail.com>
2025-06-17 10:24:06 +0200

    [TASK-24] Create DOM structure for an empty workday in Edit mode (#20)

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-workday',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './workday.page.component.html',
   styleUrl: './workday.page.component.scss',
+  host: {
+    class: 'app-workday w-100',
+  },
 })
-export default class WorkdayPageComponent {}
+export default class WorkdayPageComponent {
+  selectedDate = '2019-02-28';
+  selectedOption = '⛳️';
+  secondaryTasks = [{ name: 'Tâche n°2' }, { name: 'Tâche n°3' }];
+
+  remainingTasks = [{ name: 'Tâche n°4' }, { name: 'Tâche n°5' }, { name: 'Tâche n°6' }];
+
+  addTask() {
+    const newIndex = this.remainingTasks.length + 4;
+    this.remainingTasks.push({ name: `Tâche n°${newIndex}` });
+  }
+}

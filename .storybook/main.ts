a10415989332daec4df0fede63e65a8a3d709c42
Arnold Messifet <arnold.messifet@gmail.com>
2025-07-20 18:28:26 +0200

    chore(deps): upgrade eslint - angular-eslint - ngrx-signal V20

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/addon-a11y',
-    '@storybook/addon-docs'
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/angular',

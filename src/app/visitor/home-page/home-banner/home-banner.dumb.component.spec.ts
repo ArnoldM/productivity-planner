d62a947af9e4333e4d7e289ada1692abccb8b7d4
Arnold Messifet <arnold.messifet@gmail.com>
2025-09-08 10:16:01 +0200

    wip(test): update tests for zoneless

--- src/app/visitor/home-page/home-banner/home-banner.dumb.component.spec.ts
+++ src/app/visitor/home-page/home-banner/home-banner.dumb.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeBannerDumbComponent } from './home-banner.dumb.component';
 import { faker } from '@faker-js/faker';
